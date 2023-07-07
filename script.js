77eef52ff0844cb6ed3cb6f2379ec50f99392595
Taylor Jennings <taylor.jennings128@gmail.com>
2023-07-06 22:02:34 -0600

    html calc, css update, in process of conecting butons to screen

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+const displayValue = [];
+//add event listener on all keys that the calculator--key div handles
+keys.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    const key = e.target;
+    const action = key.dataset.action;
+
+    const display = calculator.querySelector(".calculator__display");
+    if (!action) {
+      displayValue.push("number key, ");
+      display.textContent = displayValue;
+      console.log(displayValue);
+    }
+    switch (action) {
+      case "add":
+        displayValue.push("+");
+        display.textContent = displayValue;
+        console.log("add");
+        break;
+      case "subtract":
+        console.log("subract");
+        break;
+      case "multiply":
+        console.log("multiply");
+        break;
+      case "divide":
+        console.log("divide");
+        break;
+      case "decimal":
+        console.log("decimal");
+        break;
+      case "clear":
+        console.log("clear");
+        break;
+      case "calculate":
+        console.log("equals");
+        break;
+    }
+  }
+});
+
 //basic operations
 
 const add = (x, y) => parseFloat(x + y);
